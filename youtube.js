31a6f91110e66cdb227f09c1b4b74ecf610bfbd0
Dave Winfield <dwinfield@users.noreply.github.com>
2019-12-22 00:53:34 +0000

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Youtube Disable Autoplay Related Videos
+// @match       *://*youtube.com/*
+// @grant       none
+// @version     1.0.0
+// @author      Dave Winfield
+// @description Turns off the "Autoplay" feature
+// @require     https://code.jquery.com/jquery-3.4.1.slim.min.js
+// @downloadURL https://raw.githubusercontent.com/dwinfield/userscript-youtube-autoplay/master/youtube.js
+// @supportURL  https://github.com/dwinfield/userscript-youtube-autoplay
+// @homepageURL https://github.com/dwinfield/userscript-youtube-autoplay
+// ==/UserScript==
+
+$(function() { 
+  
+  $('#toggle').click();   
+  
+});
